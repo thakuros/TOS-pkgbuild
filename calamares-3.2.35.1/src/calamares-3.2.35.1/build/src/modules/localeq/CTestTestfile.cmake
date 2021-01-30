6d08459d9ed188ff1fe80dbf4d83c1bf603416c7
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-01-30 14:01:17 +0530

    calamares pkgbuild update

---
+++ calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/localeq/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/localeq
+# Build directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/localeq
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/partition/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/partition
+# Build directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/partition
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
