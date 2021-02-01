981f9d78f8bfddac48528d79d2d1746fd6990a3e
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-02-01 10:24:27 +0530

    cleaned all folders

--- calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/initcpio/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/initcpio
-# Build directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/initcpio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(initcpiotest "/home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build/initcpiotest")
-set_tests_properties(initcpiotest PROPERTIES  ENVIRONMENT "QT_PLUGIN_PATH=/home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build:" _BACKTRACE_TRIPLES "/usr/local/share/ECM/modules/ECMAddTests.cmake;93;add_test;/home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/CMakeModules/CalamaresAddTest.cmake;34;ecm_add_test;/home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/initcpio/CMakeLists.txt;16;calamares_add_test;/home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/initcpio/CMakeLists.txt;0;")
--- calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/summary/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/src/modules/summary
-# Build directory: /home/demonkiller/Files/Projects/TOS-pkgbuild/calamares-3.2.35.1/src/calamares-3.2.35.1/build/src/modules/summary
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
