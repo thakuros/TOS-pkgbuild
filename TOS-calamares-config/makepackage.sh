56ee55e7ebcbc328f50bf1a947131a5e2464a261
demonkiller <subhadravishwakarma8gris@gmail.com>
2021-03-01 02:42:32 +0530

    uupdated calamares config pkg build

---
+++ TOS-calamares-config/makepackage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -e
+#=================================================================================
+#Author: DemonKiller
+#SPDX-License-Identifier: GPL-3.0
+#Warning! DO NOT RUN THIS SCRIPT BLINDLY. MAKE CHANGES ACCORDING TO YOUR WORKFLOW. 
+#RUN AT YOUR OWN RISK.
+#================================================================================= 
+
+rm -rvf pkg/
+rm -rvf src/
+rm -rvf TOS-calamares-config
+
+echo "Making Package..."
+makepkg
